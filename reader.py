747efee6537cbd34a70f08b6dc8a8cbf47b28951
vsem <virtualik@gmail.com>
2023-04-07 22:15:57 +0200

    reader added

--- reader.py
+++ reader.py
@@ -1,3 +1,4 @@
+from pympler import asizeof
 from typing import Generator
 
 
@@ -12,20 +13,34 @@ def read_from_file(filename: str, word: str) -> Generator:
                 yield line.replace("\n", "")
 
 
-def write_to_file(filename: str, output_filename: str, word: str):
+def write_to_file_and_count_lines(filename: str, output_filename: str, word: str) -> int:
     """This function reads data from one file and writes to another file using a generator"""
+    lines_counter: int = 0
     with open(output_filename, "wb") as output_file:
         for line in read_from_file(filename, word):
             output_file.write(bytes(f"{line}\n", encoding="utf-8"))
+            lines_counter += 1
+    return lines_counter
+
+
+def get_file_size(file_path: str) -> int:
+    """This function reads file and returns filesize in bytes"""
+    with open(file_path, "rb") as file:
+        file_obj = file.read()
+    return asizeof.asizeof(file_obj)
 
 
 def main():
     search_word: str = input("Enter the search word: ")
 
-    filename: str = "example_utf8.txt"
+    filename: str = "rockyou_utf8.txt"
     output_filename: str = "output.txt"
 
-    write_to_file(filename, output_filename, search_word)
+    lines: int = write_to_file_and_count_lines(filename, output_filename, search_word)
+
+    total_size: int = get_file_size(output_filename)
+
+    print(f"Total lines in output file: {lines}\nTotal output file size: {total_size} bytes")
 
 
 if __name__ == "__main__":
