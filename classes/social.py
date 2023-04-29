d3af09f1a058a2ba3daf15ceecfb2b8431feb96b
vsem <virtualik@gmail.com>
2023-04-29 15:50:24 +0200

    Social refactored

--- classes/social.py
+++ classes/social.py
@@ -14,61 +14,72 @@ class Creds:
 
 class SocialNetwork(ABC):
     @abstractmethod
-    def authorize(self, api_credentials: Creds) -> bool:
+    def authorize(self, api_credentials: Creds) -> None:
         """Authorize with external system."""
 
+    @property
     @abstractmethod
     def check_status(self, message_id: str) -> Any:
         """Check message status after publishing"""
 
 
 class Twitter(SocialNetwork):
-    def authorize(self, api_credentials: Creds) -> bool:
-        try:
-            auth = tweepy.OAuthHandler(
-                api_credentials.get("consumer_key"),
-                api_credentials.get("consumer_secret"),
-            )
-            auth.set_access_token(
-                api_credentials.get("access_token"),
-                api_credentials.get("access_token_secret"),
-            )
-            self.auth = auth
-            self.api = tweepy.API(auth)
-            return True
+    api = None
 
-        except Exception as e:
-            raise Exception(e)
+    def authorize(self, api_credentials: Creds) -> None:
+        auth = tweepy.OAuthHandler(
+            api_credentials.get("consumer_key"),
+            api_credentials.get("consumer_secret"),
+        )
+        auth.set_access_token(
+            api_credentials.get("access_token"),
+            api_credentials.get("access_token_secret"),
+        )
+        self.api = tweepy.API(auth)
 
+    @property
     def check_status(self, message_id: str):
-        status = self.api.get_status(message_id)
-        return status._json['text']
+        ...
 
 
 class Facebook(SocialNetwork):
+    graph = None
+
     def authorize(self, api_credentials: Creds) -> None:
-        graph = facebook.GraphAPI(access_token=api_credentials.get("access_token"))
-        self.graph = graph
+        self.graph = facebook.GraphAPI(access_token=api_credentials.get("access_token"))
+
+    @property
+    def check_status(self, message_id: str):
+        ...
 
 
 class YouTube(SocialNetwork):
+    youtube = None
+
     def authorize(self, api_credentials: Creds) -> None:
-        youtube = build("youtube", "v3", developerKey=api_credentials.get("api_key"))
-        self.youtube = youtube
+        self.youtube = build(
+            "youtube", "v3", developerKey=api_credentials.get("api_key")
+        )
+
+    @property
+    def check_status(self, message_id: str):
+        ...
 
 
 class Post:
     def __init__(self, social_network: SocialNetwork) -> None:
-        self.social_network = social_network
+        self._social_network = social_network
 
     def post_message(self, message: str) -> dict[str, str]:
-        if isinstance(self.social_network, Twitter):
-            return self.social_network.api.update_status(message)
-        elif isinstance(self.social_network, Facebook):
-            return self.social_network.graph.put_object(
+        if isinstance(self._social_network, Twitter):
+            return self._social_network.api.update_status(message)
+
+        if isinstance(self._social_network, Facebook):
+            return self._social_network.graph.put_object(
                 parent_object="me", connection_name="feed", message=message
             )
-        elif isinstance(self.social_network, YouTube):
+
+        if isinstance(self._social_network, YouTube):
             video = {
                 "snippet": {
                     "title": "New video",
@@ -79,42 +90,44 @@ class Post:
                 "status": {"privacyStatus": "public"},
             }
             response = (
-                self.social_network.youtube.videos()
+                self._social_network.youtube.videos()
                 .insert(part="snippet,status", body=video, media_body=None)
                 .execute()
             )
             return response
 
+        return {"error": "Not supported social network."}
 
-twitter_api_credentials: dict[str, str] = {
-    "consumer_key": "your_consumer_key",
-    "consumer_secret": "your_consumer_secret",
-    "access_token": "your_access_token",
-    "access_token_secret": "your_access_token_secret",
-}
 
-facebook_api_credentials: dict[str, str] = {
-    "access_token": "your_access_token",
-}
+def main():
+    twitter_api_credentials = {
+        "consumer_key": "your_consumer_key",
+        "consumer_secret": "your_consumer_secret",
+        "access_token": "your_access_token",
+        "access_token_secret": "your_access_token_secret",
+    }
 
-youtube_api_credentials: dict[str, str] = {
-    "api_key": "your_youtube_api_key",
-}
+    facebook_api_credentials = {
+        "access_token": "your_access_token",
+    }
 
-twitter = Twitter()
-twitter.authorize(twitter_api_credentials)
+    youtube_api_credentials = {
+        "api_key": "your_youtube_api_key",
+    }
 
-fb = Facebook()
-fb.authorize(facebook_api_credentials)
+    tw = Twitter()
+    tw.authorize(twitter_api_credentials)
 
-youtube = YouTube()
-youtube.authorize(youtube_api_credentials)
+    fb = Facebook()
+    fb.authorize(facebook_api_credentials)
 
-twitter_post = Post(twitter)
-facebook_post = Post(fb)
-youtube_post = Post(youtube)
+    yt = YouTube()
+    yt.authorize(youtube_api_credentials)
 
-twitter_response = twitter_post.post_message("Hello, Twitter!")
-print(f"Twitter response: {twitter_response}")
+    Post(tw).post_message("twitter message")
+    Post(fb).post_message("facebook message")
+    Post(yt).post_message("youtube message")
 
-facebook_response = facebook_post.post_message
+
+if __name__ == "__main__":
+    main()
