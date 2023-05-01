ed4ee27adee7776adad81bcddb9c2de64262e354
vsem <virtualik@gmail.com>
2023-05-01 15:25:56 +0200

    everything fixed

--- classes/social_hero.py
+++ classes/social_hero.py
@@ -1,52 +1,102 @@
 from abc import ABC, abstractmethod
-from datetime import datetime, timedelta
+from dataclasses import dataclass
+from datetime import datetime
+from datetime import time as t
 from typing import List
 
 
+@dataclass
+class Creds:
+    creds: dict[str, str]
+
+
+class Timer:
+    def __init__(self, hh=None, mm=None, ss=None):
+        if hh and mm and ss:
+            self.time = int(datetime.combine(datetime.today(), t(hour=hh, minute=mm, second=ss)).timestamp() * 1000)
+        else:
+            self.time = int(datetime.now().timestamp() * 1000)
+
+    def __str__(self):
+        return datetime.fromtimestamp(self.time / 1000).strftime("%Y-%m-%d %H:%M:%S")
+
+    def __eq__(self, other):
+        return self.time == other.time
+
+    def __ne__(self, other):
+        return self.time != other.time
+
+    def __lt__(self, other):
+        return self.time < other.time
+
+    def __gt__(self, other):
+        return self.time > other.time
+
+    def __le__(self, other):
+        return self.time <= other.time
+
+    def __ge__(self, other):
+        return self.time >= other.time
+
+
+@dataclass
+class Post:
+    message: str
+    timestamp: Timer
+
+
 class SocialChannel(ABC):
-    def __init__(self, followers: int):
+    def __init__(self, followers: int = 0):
         self.followers = followers
 
     @abstractmethod
-    def authorize(self, creds: dict[str, str]):
+    def authorize(self, creds: dict[str, str]) -> None:
         """Authorize with external system."""
 
     @abstractmethod
-    def post(self, message: str):
+    def post(self, post: Post) -> None:
         """Post message with external system"""
 
 
 class YoutubeChannel(SocialChannel):
-    def authorize(self, creds: str):
-        print(f"{creds=}")
+    def authorize(self, creds: str) -> None:
+        print("Authorized")
 
-    def post(self, message: str):
-        print(f"{message=}")
+    def post(self, post: Post) -> None:
+        print(f"{post.message} posted at {post.timestamp} in YouTube")
 
 
 class FacebookChannel(SocialChannel):
-    def authorize(self, creds: str):
-        print(f"{creds=}")
+    def authorize(self, creds: str) -> None:
+        print("Authorized")
 
-    def post(self, message: str):
-        print(f"{message=}")
+    def post(self, post: Post) -> None:
+        print(f"{post.message} posted at {post.timestamp} in Facebook")
 
 
 class TwitterChannel(SocialChannel):
-    def authorize(self, creds: str):
-        print(f"{creds=}")
+    def authorize(self, creds: str) -> None:
+        print("Authorized")
 
-    def post(self, message: str):
-        print(f"{message=}")
-
-
-class Post:
-    def __init__(self, message: str, timestamp: datetime):
-        self.message = message
-        self.timestamp = timestamp
+    def post(self, post: Post) -> None:
+        print(f"{post.message} posted at {post.timestamp} in Twitter")
 
 
-def post_a_message(channel: SocialChannel, message: str) -> None:
+def social_channel_dispatcher(decision: str) -> SocialChannel:
+    if decision.lower() == "youtube":
+        return YoutubeChannel()
+    if decision.lower() == "facebook":
+        return FacebookChannel()
+    if decision.lower() == "twitter":
+        return TwitterChannel()
+    raise ValueError("Social Channel not found!")
+
+
+def post_a_message(channel: str, message: Post, **kwargs) -> None:
+    creds = kwargs.get("creds")
+    channel= social_channel_dispatcher(channel)
+    if creds:
+        authorize(channel, creds)
     channel.post(message)
 
 
@@ -54,30 +104,34 @@ def authorize(channel: SocialChannel, creds: dict[str, str]) -> None:
     channel.authorize(creds)
 
 
-def process_schedule(posts: List[Post], channels: List[SocialChannel]) -> None:
+def process_schedule(posts: List[Post], channels: List[str]) -> None:
     for post in posts:
-        if post.timestamp >= datetime.now():
+        if post.timestamp <= Timer():
             for channel in channels:
-                post_a_message(channel, post.message)
+                post_a_message(channel, post)
 
 
 def main():
-    # create social channel objects
-    youtube_channel = YoutubeChannel(1000)  # 1000 followers
-    facebook_channel = FacebookChannel(500)  # 500 followers
-    twitter_channel = TwitterChannel(200)  # 200 followers
+    print("************** Regular post ****************")
 
-    # create list of social channel objects
-    channels = [youtube_channel, facebook_channel, twitter_channel]
+    youtube_post = Post("YouTube regular post", Timer())
+    post_a_message("youtube", youtube_post)
 
-    # create list of posts
+    facebook_post = Post("Facebook regular post", Timer())
+    post_a_message("facebook", facebook_post)
+
+    twitter_post = Post("Twitter regular post", Timer())
+    post_a_message("twitter", twitter_post)
+
+    print("\n************** Scheduled post ****************")
+
+    channels = ["youtube", "facebook", "twitter"]
+
     posts = [
-        Post("Check out my latest video!", datetime.now() + timedelta(seconds=60)),  # post in 1 minute
-        Post("Join me for a live stream!", datetime.now() + timedelta(seconds=120)),  # post in 2 minutes
-        Post("New blog post is up!", datetime.now() + timedelta(seconds=180)),  # post in 3 minutes
+        Post("Regular - Check out my post now!", Timer()),
+        Post("Scheduled - Join me for a while!", Timer(11, 11, 11)),
     ]
 
-    # schedule posts
     process_schedule(posts, channels)
 
 
